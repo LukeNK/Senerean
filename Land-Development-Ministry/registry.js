260da4ec4a241b3ecca66a46308eb15161aa57e4
Luke Nguyen Khanh <lukenguyenkhanh@gmail.com>
2023-01-25 23:23:49 -0800

    Added land registry Include most of builds on the north side of Uphill st

---
+++ Land-Development-Ministry/registry.js
@@ -0,0 +1,258 @@
+let REGISTRY = [
+    {
+        "id": "FIRSTB",
+        "owner": "LukeNK",
+        "desc": "Modern seaside house",
+        "chunks": [
+            {
+                "x": 5,
+                "z": -4
+            },
+            {
+                "x": 5,
+                "z": -3
+            }
+        ]
+    },
+    {
+        "id": "NCLIFF",
+        "owner": "LukeNK",
+        "desc": "Wooden mansion",
+        "chunks": [
+            {
+                "x": 2,
+                "z": -4
+            },
+            {
+                "x": 2,
+                "z": -3
+            },
+            {
+                "x": 3,
+                "z": -4
+            },
+            {
+                "x": 3,
+                "z": -3
+            }
+        ]
+    },
+    {
+        "id": "FHOUSE",
+        "owner": "LukeNK",
+        "desc": "Modern house",
+        "chunks": [
+            {
+                "x": 3,
+                "z": -2
+            },
+            {
+                "x": 4,
+                "z": -2
+            }
+        ]
+    },
+    {
+        "id": "GREENC",
+        "owner": "LukeNK",
+        "desc": "Small house",
+        "chunks": [
+            {
+                "x": 4,
+                "z": -2
+            }
+        ]
+    },
+    {
+        "id": "MLAFCE",
+        "owner": "LukeNK",
+        "desc": "Wooden house",
+        "chunks": [
+            {
+                "x": 4,
+                "z": -3
+            },
+            {
+                "x": 5,
+                "z": -3
+            }
+        ]
+    },
+    {
+        "id": "FARMHO",
+        "owner": "LukeNK",
+        "desc": "Wooden house with roof farm",
+        "chunks": [
+            {
+                "x": 5,
+                "z": -3
+            }
+        ]
+    },
+    {
+        "id": "SEAAPP",
+        "owner": "LukeNK",
+        "desc": "Apartment for 3 with farm",
+        "chunks": [
+            {
+                "x": 5,
+                "z": -3
+            },
+            {
+                "x": 6,
+                "z": -3
+            }
+        ]
+    },
+    {
+        "id": "FAJOPS",
+        "owner": "LukeNK",
+        "desc": "Single family home",
+        "chunks": [
+            {
+                "x": 5,
+                "z": -3
+            },
+            {
+                "x": 5,
+                "z": -2
+            },
+            {
+                "x": 6,
+                "z": -3
+            },
+            {
+                "x": 6,
+                "z": -2
+            }
+        ]
+    },
+    {
+        "id": "APPF7P",
+        "owner": "LukeNK",
+        "desc": "Apartment for 7",
+        "chunks": [
+            {
+                "x": 4,
+                "z": -2
+            },
+            {
+                "x": 4,
+                "z": -1
+            }
+        ]
+    },
+    {
+        "id": "F3PV9K",
+        "owner": "LukeNK",
+        "desc": "Small wooden house",
+        "chunks": [
+            {
+                "x": 5,
+                "z": -3
+            },
+            {
+                "x": 5,
+                "z": -2
+            }
+        ]
+    },
+    {
+        "id": "H5JO3K",
+        "owner": "LukeNK",
+        "desc": "Storage house with ship dock",
+        "chunks": [
+            {
+                "x": 6,
+                "z": -3
+            },
+            {
+                "x": 6,
+                "z": -2
+            },
+            {
+                "x": 7,
+                "z": -3
+            }
+        ]
+    },
+    {
+        "id": "J8KO1P",
+        "owner": "LukeNK",
+        "desc": "Restaurant",
+        "chunks": [
+            {
+                "x": 5,
+                "z": -2
+            },
+            {
+                "x": 6,
+                "z": -2
+            }
+        ]
+    },
+    {
+        "id": "I9FJ1A",
+        "owner": "LukeNK",
+        "desc": "Coffee shop",
+        "chunks": [
+            {
+                "x": 6,
+                "z": -2
+            },
+            {
+                "x": 7,
+                "z": -2
+            }
+        ]
+    },
+    {
+        "id": "CLHOSP",
+        "owner": "Senerean",
+        "desc": "Three-floor hospital with ER connected to the street",
+        "chunks": [
+            {
+                "x": 1,
+                "z": -2
+            },
+            {
+                "x": 2,
+                "z": -2
+            }
+        ]
+    },
+    {
+        "id": "VACIMU",
+        "owner": "Senerean",
+        "desc": "Vaccine and imunization clinic",
+        "chunks": [
+            {
+                "x": 1,
+                "z": -2
+            }
+        ]
+    },
+    {
+        "id": "TRRAIL",
+        "owner": "Senerean",
+        "desc": "Train manufacturing facility with showroom",
+        "chunks": [
+            {
+                "x": 0,
+                "z": -3
+            },
+            {
+                "x": 0,
+                "z": -2
+            },
+            {
+                "x": 1,
+                "z": -3
+            },
+            {
+                "x": 1,
+                "z": -2
+            }
+        ]
+    },
+]
---
+++ Land-Development-Ministry/template.js
@@ -0,0 +1,6 @@
+const TEMPLATE = {
+    id: 'ID of the build (think it as number plate for builds)',
+    owner: 'The name of the current owner',
+    desc: 'Breif summary of the build',
+    chunks: 'Chunks that have the build (using x, z format), leave empty if all were listed'
+}
