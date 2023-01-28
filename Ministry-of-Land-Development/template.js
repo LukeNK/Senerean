cf849c0385164bb3fd791e69a6961d9c58faaaee
Luke Nguyen Khanh <lukenguyenkhanh@gmail.com>
2023-01-27 22:49:38 -0800

    Added options for download Added auto-generated ID

--- Ministry-of-Land-Development/template.js
+++ Ministry-of-Land-Development/template.js
@@ -1,5 +1,5 @@
 const TEMPLATE = {
-    id: 'ID of the build (think it as number plate for builds)',
+    id: 'ID of the build, type a dot "." to auto-generate (think it as number plate for builds)',
     owner: 'The name of the current owner',
     desc: 'Breif summary of the build',
     address: "Road base on main entrance, not include house number (you don't have it anyways)",
@@ -11,3 +11,6 @@ const ROAD_TYPE = [
     'st',
     'ave',
 ]
+
+// characters permitted for ID
+const ALLOW_CHAR = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
