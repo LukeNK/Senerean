125ad2ae335ec0210fb4aed8797d6c61fefd44d0
Luke Nguyen Khanh <lukenguyenkhanh@gmail.com>
2023-02-12 23:53:58 -0800

    Ministry of Land Development homepage

--- Ministry-of-Land-Development/scripts.js
+++ Ministry-of-Land-Development/scripts.js
@@ -1,17 +1,19 @@
 function taxReport(owner) {
     // simple script to check land status of an owner
     let area = 0, // total owned area
-        builds = [], // list of all builds
+        builds = [], // list of all taxable builds
         totalBuilds = 0, // total number of builds
         totalTax = 0;
     for (let build of REGISTRY) 
         if (build.owner == owner) {
             // if there is dimension (to supports legacy database)
             if (build.dimensions) {
-                builds.push(build.id);
+                builds.push(build.id + ' = ');
                 let buildDim = build.dimensions.x * build.dimensions.z;
                 area += buildDim;
-                totalTax += landTax(buildDim)
+                totalTax += devTax(buildDim);
+                builds[builds.length - 1] = 
+                    builds.at(-1) + devTax(buildDim).toFixed(2)
             }
             totalBuilds++
         }
@@ -21,22 +23,45 @@ function taxReport(owner) {
 # ${new Date().toLocaleDateString()}
 Total number of taxable builds: ${builds.length}
 Total area covered: ${area} blocks^2
-Total tax due: ${(totalTax.toFixed(2))} Seners
 List of all builds that are taxable (${builds.length}/${totalBuilds}):
-${builds.join(', ')}`
+${builds.join('\n')}
+Total  = ${totalTax.toLocaleString('en-CA', 
+    {
+        minimumFractionDigits: 2, 
+        maximumFractionDigits:2
+    })
+} Seners`
 
     return report;
 }
 
-function idSearch(query) {
-    // search for ID, return all matching builds
+function buildSearch(query) {
+    // search for build, return all matching builds
     let builds = [];
-    for (const build of REGISTRY)
-        if (build.id.includes(query)) builds.push(build);
+    if (query.includes(',')) {
+        // if there is a comma, that mean user is searching for cord
+        query = query.split(',');
+        const x = query[0], z = query[1];
+        for (const build of REGISTRY) {
+            let nw = build.nw || 0, 
+                se = build.se || 0;
+            if (!nw || !se) continue; // sorry legacy builds
+            if (
+                nw.x < x && x < se.x
+                && nw.z < z && z < se.z
+            ) builds.push(build);
+        }
+    } else // normal phrase search
+        for (const build of REGISTRY)
+            if (
+                build.id.includes(query.toUpperCase())
+                || build.desc.toUpperCase().includes(query.toUpperCase())
+                || build.owner.toUpperCase() == query.toUpperCase()
+            ) builds.push(build);
     return builds;
 }
 
-function landTax(area) {
+function devTax(area) {
     // function to calculate tax PER BUILD
     // please use desmos before changing values here
     const fixedRate = 100, // fixed rate for every builds 
