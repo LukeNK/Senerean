46c293040f17889b999979a7d931d2b2b8595b22
Luke Nguyen Khanh <lukenguyenkhanh@gmail.com>
2023-08-27 09:39:10 +0700

    Added script to generate MRZ for TD1 Removed link shorterner to put it as root

--- website/scripts.js
+++ website/scripts.js
@@ -39,7 +39,7 @@ function setCookie(cname, cvalue, exdays) {
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
     let expires = "expires="+d.toUTCString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-  }
+}
   
 function getCookie(cname) {
     let name = cname + "=";
