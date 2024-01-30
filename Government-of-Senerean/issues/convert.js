e61e18917cb16bf543510c8727d4ae747dee4fea
Luke Nguyen Khanh <lukenguyenkhanh@gmail.com>
2024-01-30 23:23:53 +0700

    EOL problem

--- Government-of-Senerean/issues/convert.js
+++ Government-of-Senerean/issues/convert.js
@@ -3,13 +3,12 @@ const fs = require('fs'),
     issue = 0;
 
 let data = fs.readFileSync(issue + '.md', 'utf-8');
-data = data.split('\n');
+data = data.split('\r\n');
 
-console.log(data)
 // replace line
 for (const l1 in data)
     if (data[l1].startsWith('#'))
         data[l1] = '[b]' + data[l1] + '[/b]';
 
-data = data.join('\n')
+data = data.join('\r\n')
 fs.writeFileSync('ns.md', data, 'utf-8');
